7ce958a3bc28e93d665f2e57058db6d75bb35449
rishikesh200 <93971335+rishikesh200@users.noreply.github.com>
2024-03-21 13:10:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var bt =document.querySelector("#tom");
+var list =document.querySelector("#lim");
+
+bt.addEventListener('click',()=>{
+    
+    console.log('click');
+    list.classList.toggle('togle');
+})
+
+
